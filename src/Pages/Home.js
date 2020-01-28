145f3dbd94cf3d6a473bb033403f7bb8ada0da3b
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-28 16:43:35 -0700

    stickler 1643

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 
-const HomePage = () => { (
-    <Container>
-      Home test
-    </Container>
-  );
+const HomePage = () => {
+  (
+  <Container>
+    Home test
+  </Container>
+);
 };
 
 export default HomePage;
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,53 +1,54 @@
 import React from 'react';
-import { Button, Form, Grid, Header, Segment, Icon, } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Segment, Icon } from 'semantic-ui-react';
 
 const SignupPage = () => {
-  return (
-    <Grid textAlign="center" style={{ height: "100vh", marginTop: "2em" }}>
+  (
+    <Grid textAlign='center' style={{ height: '100vh', marginTop: '2em' }}>
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" style={{color:"#59499e"}} textAlign="center">
-          <Icon name="user" /> Create your account
+        <Header as='h2' style={{ color:'#59499e' }} textAlign='center'>
+          <Icon name='user' /> 
+          Create your account
         </Header>
-        <Form size="large">
+        <Form size='large'>
           <Segment stacked>
-            <label htmlFor="name">
+            <label htmlFor='name'>
               <Form.Input
                 fluid
-                icon="user"
-                iconPosition="left"
-                id="name"
-                type="text"
-                name="name"
-                placeholder="Name"
-                style={{marginBottom: "1em"}}
+                icon='user'
+                iconPosition='left'
+                id='name'
+                type='text'
+                name='name'
+                placeholder='Name'
+                style={{ marginBottom: '1em' }}
 
               />
             </label>
-            <label htmlFor="email">
+            <label htmlFor='email'>
               <Form.Input
                 fluid
-                icon="user"
-                iconPosition="left"
-                id="email"
-                type="text"
-                name="email"
-                placeholder="Email"
-                style={{marginBottom: "1em"}}
+                icon='user'
+                iconPosition='left'
+                id='email'
+                type='text'
+                name='email'
+                placeholder='Email'
+                style={{ marginBottom: '1em' }}
 
               />
             </label>
-            <label htmlFor="password">
+            <label htmlFor='password'>
               <Form.Input
                 fluid
-                icon="lock"
-                iconPosition="left"
-                type="password"
-                name="password"
-                placeholder="Password"
-                style={{marginBottom: "1em"}}
+                icon='lock'
+                iconPosition='left'
+                type='password'
+                name='password'
+                placeholder='Password'
+                style={{ marginBottom: '1em' }}
               />
             </label>
-            <Button style={{backgroundColor: "#5cba57", color: "white"}} fluid size="large" type="submit">
+            <Button style={{ backgroundColor: '#5cba57', color: 'white' }} fluid size='large' type='submit'>
               Signup
             </Button>
           </Segment>

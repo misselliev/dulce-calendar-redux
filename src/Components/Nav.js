d2422949b9a301e7a097b0befc0357aa5f8f6c56
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-28 14:17:30 -0700

    Setting up navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import userActions from '../redux/userActions';
 import {
   Container,
   Menu,
@@ -9,32 +7,14 @@ import {
 
 const Nav = () => {
   const linkStyle = { color: '#C513AF', fontWeight: 'bold', fontSize: '1.2em' };
-  const dispatch = useDispatch();
-  const handleLogout = () => {
-    dispatch(userActions.logoutUser());
-  };
-  const user = useSelector(state => state.user);
-  const display = user.currentUser ? (
-    <Menu stackable widths={4}>
-      <Menu.Item><Link style={linkStyle} to="/">Home</Link></Menu.Item>
-      <Menu.Item><Link style={linkStyle} to="/listings">Listings</Link></Menu.Item>
-      <Menu.Item><Link style={linkStyle} to="/favorites">Favorites</Link></Menu.Item>
-      <Menu.Item>
-        <Link style={linkStyle} to="/" onClick={handleLogout}>
-        Logout
-        </Link>
-      </Menu.Item>
-    </Menu>
-  ) : (
-    <Menu stackable widths={3}>
-      <Menu.Item><Link style={linkStyle} to="/">Home</Link></Menu.Item>
-      <Menu.Item><Link style={linkStyle} to="/signup">Signup</Link></Menu.Item>
-      <Menu.Item><Link style={linkStyle} to="/login">Login</Link></Menu.Item>
-    </Menu>
-  );
   return (
     <Container>
-      {display}
+      <Menu stackable widths={4}>
+        <Menu.Item><Link style={linkStyle} to="/">Home</Link></Menu.Item>
+        <Menu.Item><Link style={linkStyle} to="/login">Login</Link></Menu.Item>
+        <Menu.Item><Link style={linkStyle} to="/signup">Signup</Link></Menu.Item>
+        <Menu.Item><Link style={linkStyle} to="/">Logout</Link></Menu.Item>
+      </Menu>
     </Container>
   );
 };

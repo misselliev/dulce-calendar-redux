b2a2919d17dfa3146440a0e51c7da3adc2c702da
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-28 14:19:49 -0700

    Initializing: redux store, rootReducer and userReducer. Setting up redux dev tools

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,14 +1,14 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './redux/rootReducer';
+import rootReducer from './rootReducer';
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const initState = {
   user: { currentUser: false },
-  listings: {},
-  favorites: {},
+  talks: {},
+  schedule: {},
 };
 const store = createStore(rootReducer, initState, composeEnhancer(applyMiddleware(thunk)));
 

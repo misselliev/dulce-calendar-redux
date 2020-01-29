426e13d5c222787a4185180db9dd624e917a4b9c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-28 19:34:17 -0700

    Adding handleLogout to logout link

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import userActions from '../Redux/userActions';
 import {
   Container,
   Menu,
 } from 'semantic-ui-react';
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(userActions.logoutUser());
+  };
+
   const linkStyle = { color: '#59499e', fontWeight: 'bold', fontSize: '1.2em' };
   return (
     <Container>
@@ -13,7 +20,7 @@ const Nav = () => {
         <Menu.Item><Link style={linkStyle} to="/">Home</Link></Menu.Item>
         <Menu.Item><Link style={linkStyle} to="/login">Login</Link></Menu.Item>
         <Menu.Item><Link style={linkStyle} to="/signup">Signup</Link></Menu.Item>
-        <Menu.Item><Link style={linkStyle} to="/">Logout</Link></Menu.Item>
+        <Menu.Item><Link style={linkStyle} to="/" onClick={handleLogout}>Logout</Link></Menu.Item>
       </Menu>
     </Container>
   );

68772c5203ad9d4fbc22af263a8781261d25f290
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-28 19:35:22 -0700

    Adding react-redux hooks, handleSubmit and handleChange implemented

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   Button, Form, Grid, Header, Segment, Icon,
 } from 'semantic-ui-react';
+import userActions from '../Redux/userActions';
 
-const LoginPage = () => {
-  (
+const LoginPage = (props) => {
+  const dispatch = useDispatch();
+
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(userActions.loginUser(loginForm));
+    props.history.push('/');
+  };
+
+  const handleChange = e => setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+
+  const { email, password } = loginForm;
+
+  return (
     <Grid textAlign="center" style={{ height: '100vh', marginTop: '2em' }}>
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" style={{ color: '#59499e' }} textAlign="center">
           <Icon name="home" />
           Log-in to your account
         </Header>
-        <Form size="large">
+        <Form size="large" onSubmit={handleSubmit}>
           <Segment stacked>
-            <Form.Field
+            <Form.Input
               id="form-input-control-email"
               fluid
               icon="user"
               iconPosition="left"
               type="text"
               name="email"
+              value={email}
+              onChange={ handleChange }
               placeholder="Email"
               label="Email"
               style={{ marginBottom: '1em' }}
             />
-            <Form.Field
+            <Form.Input
               id="form-input-control-password"
               fluid
               icon="lock"
               iconPosition="left"
               type="password"
               name="password"
+              value={password}
+              onChange={ handleChange }
               placeholder="Password"
               label="Password"
               style={{ marginBottom: '1em' }}

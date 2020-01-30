247597e7ee36e2ca5e1af462eeee2c9024421791
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-29 18:26:46 -0700

    Adding routing for Talks page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,11 @@
 import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
+import Talks from './Talks';
 
 export default {
   Login,
   Signup,
   Home,
+  Talks,
 };
--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ const Routes = () => (
   <Switch>
     <Route path="/signup" component={Pages.Signup} />
     <Route path="/login" component={Pages.Login} />
+    <Route path="/talks" component={Pages.Talks} />
     <Route exact path="/" component={Pages.Home} />
   </Switch>
 );

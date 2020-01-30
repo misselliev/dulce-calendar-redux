139c1df1881f233f9f366555202b3aa8a4a6d2dc
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-30 13:26:00 -0700

    stickler 1325

--- src/Pages/Talks.js
+++ src/Pages/Talks.js
@@ -1,37 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { Container, Item, Segment, Button } from 'semantic-ui-react';
+import {
+  Container, Item, Segment, Button
+} from 'semantic-ui-react';
 import talksActions from '../Redux/talksActions';
 
 const TalkPage = () => {
   const talks = useSelector(state => state.talks.talks, shallowEqual) || [];
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(talksActions.fetchAllTalks());
-  }, [dispatch])
+  }, [dispatch]);
 
   const style = {
-  button: {
-    color: 'white',
-    backgroundColor: '#5cba57',
-    margin: '0.1em 0.01em 0.1em 0.5em',
-    width: '6em',
-    padding: '1em',
-    minHeight: '100%',
-  },
-  item: {
-    display: 'flex',
-  },
-
-}
+    button: {
+      color: 'white',
+      backgroundColor: '#5cba57',
+      margin: '0.1em 0.01em 0.1em 0.5em',
+      width: '6em',
+      padding: '1em',
+      minHeight: '100%',
+    },
+    item: {
+      display: 'flex',
+    },
+  };
 
   return (
-  <Container>
+    <Container>
     <h1>Talks Calendar:</h1>
     <React.Fragment>
       <Item.Group divided>
-      {talks.map(({
+        {talks.map(({
         id,
         title,
         description,
@@ -41,47 +42,46 @@ const TalkPage = () => {
         speaker_name,
         speaker_title,
       }) => (
-          <Segment raised color="violet" key={id}>
-          <Item style={style.item}>
-        <React.Fragment>
+        <Segment raised color="violet" key={id}>
+            <Item style={style.item}>
+            <React.Fragment>
           <Item.Content>
-          <Item.Header as='h2'>{title}</Item.Header>
-          <Item.Description>
-          <p>
-            Description:
-            {' ' + description}
-          </p>
-          <p>
-            Location:
-            {' ' + location}
-          </p>
-          <p>
-            Date:
-            {date}
-          </p>
-          <p>
-            Time:
-            {' ' + time.substring(11, 16)}
-          </p>
-          <p>
-            Speaker name:
-            {' ' + speaker_name}
-          </p>
-          <p>
-            Speaker title:
-            {' ' + speaker_title}
-          </p>
+            <Item.Header as="h2">{title}</Item.Header>
+            <Item.Description>
+            <p>
+              Description:
+              {' ' + description}
+            </p>
+            <p>
+              Location:
+              {' ' + location}
+            </p>
+            <p>
+              Date:
+              {date}
+            </p>
+            <p>
+              Time:
+              {' ' + time.substring(11, 16)}
+            </p>
+            <p>
+              Speaker name:
+              {' ' + speaker_name}
+            </p>
+            <p>
+              Speaker title:
+              {' ' + speaker_title}
+            </p>
           </Item.Description>
           </Item.Content>
         </React.Fragment>
-        <React.Fragment>
+            <React.Fragment>
           <div>
-          <Button style={style.button}>Add to Calendar</Button>
-          
+            <Button style={style.button}>Add to Calendar</Button>
           </div>
         </React.Fragment>
-        </Item>
-        </Segment>
+            </Item>
+            </Segment>
       ))}
       </Item.Group>
     </React.Fragment>
--- src/Redux/talksActions.js
+++ src/Redux/talksActions.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const setTalks = (talks) => ({
+const setTalks = talks => ({
   type: 'SET_TALKS',
   payload: talks,
-})
+});
 
 const fetchAllTalks = () => (dispatch) => {
-  axios.get('/api/v1/talks').then(res => {
-  dispatch(setTalks(res.data));
-});
+  axios.get('/api/v1/talks').then((res) => {
+    dispatch(setTalks(res.data));
+  });
 };
 
 export default {
   fetchAllTalks,
-}
+};

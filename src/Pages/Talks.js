f260c3befd0908054254cde97212b5a01381a275
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-30 13:35:58 -0700

    stickler 1335

--- src/Pages/Talks.js
+++ src/Pages/Talks.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import {
-  Container, Item, Segment, Button
+  Container, Item, Segment, Button,
 } from 'semantic-ui-react';
 import talksActions from '../Redux/talksActions';
 
@@ -29,63 +29,57 @@ const TalkPage = () => {
 
   return (
     <Container>
-    <h1>Talks Calendar:</h1>
-    <React.Fragment>
+      <h1>Talks Calendar:</h1>
+      <React.Fragment>
       <Item.Group divided>
         {talks.map(({
-        id,
-        title,
-        description,
-        location,
-        date,
-        time,
-        speaker_name,
-        speaker_title,
-      }) => (
+          id,
+          title,
+          description,
+          location,
+          date,
+          time,
+          speaker_name,
+          speaker_title,
+        }) => (
         <Segment raised color="violet" key={id}>
-            <Item style={style.item}>
-            <React.Fragment>
-          <Item.Content>
+          <Item style={style.item}>
+              <React.Fragment>
+              <Item.Content>
             <Item.Header as="h2">{title}</Item.Header>
             <Item.Description>
-            <p>
-              Description:
-              {' ' + description}
-            </p>
-            <p>
-              Location:
-              {' ' + location}
-            </p>
-            <p>
-              Date:
-              {date}
-            </p>
-            <p>
-              Time:
-              {' ' + time.substring(11, 16)}
-            </p>
-            <p>
-              Speaker name:
-              {' ' + speaker_name}
-            </p>
-            <p>
-              Speaker title:
-              {' ' + speaker_title}
-            </p>
-          </Item.Description>
+              <p>
+              Description: {description}
+              </p>
+              <p>
+              Location: {location}
+              </p>
+              <p>
+              Date: {date}
+              </p>
+              <p>
+              Time: {time.substring(11, 16)}
+              </p>
+              <p>
+              Speaker: {speaker_name}
+              </p>
+              <p>
+              Speaker title: {speaker_title}
+              </p>
+            </Item.Description>
           </Item.Content>
-        </React.Fragment>
-            <React.Fragment>
-          <div>
-            <Button style={style.button}>Add to Calendar</Button>
-          </div>
-        </React.Fragment>
+            </React.Fragment>
+              <React.Fragment>
+              <div>
+                <Button style={style.button}>Add to Calendar</Button>
+              </div>
+            </React.Fragment>
             </Item>
             </Segment>
-      ))}
+        ))}
       </Item.Group>
     </React.Fragment>
-  </Container>
+    </Container>
   );
 };
 

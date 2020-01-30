b899b44f02e4fd3f7b16dd045124831b55973ece
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-30 14:07:50 -0700

    stickler 1407

--- src/Pages/Talks.js
+++ src/Pages/Talks.js
@@ -45,41 +45,41 @@ const TalkPage = () => {
             <Segment raised color="violet" key={id}>
               <Item style={style.item}>
                 <React.Fragment>
-                <Item.Content>
-                  <Item.Header as="h2">{title}</Item.Header>
-                  <Item.Description>
-                      <p>
-                      Description:
-                      {description}
-                      </p>
-                      <p>
-                      Location:
-                      {location}
-                      </p>
-                      <p>
-                      Date:
-                      {date}
-                      </p>
-                      <p>
-                      Time:
-                      {time.substring(11, 16)}
-                      </p>
-                      <p>
-                      Speaker:
-                      {speaker_name}
-                      </p>
-                      <p>
-                      Speaker title:
-                      {speaker_title}
-                      </p>
-                  </Item.Description>
-                </Item.Content>
+                  <Item.Content>
+                    <Item.Header as="h2">{title}</Item.Header>
+                    <Item.Description>
+                    <p>
+                        Description:
+                        {description}
+                    </p>
+                    <p>
+                        Location:
+                        {location}
+                    </p>
+                    <p>
+                        Date:
+                        {date}
+                    </p>
+                    <p>
+                        Time:
+                        {time.substring(11, 16)}
+                    </p>
+                    <p>
+                        Speaker:
+                        {speaker_name}
+                    </p>
+                    <p>
+                        Speaker title:
+                        {speaker_title}
+                    </p>
+                    </Item.Description>
+                  </Item.Content>
                 </React.Fragment>
-            <React.Fragment>
-                <div>
-                  <Button style={style.button}>Add to Calendar</Button>
-                </div>
-            </React.Fragment>
+                <React.Fragment>
+              <div>
+                <Button style={style.button}>Add to Calendar</Button>
+              </div>
+                </React.Fragment>
               </Item>
             </Segment>
           ))}

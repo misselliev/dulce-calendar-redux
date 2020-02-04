edc972177bb10eec8aa5154f40518aaa11129568
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-04 14:56:33 -0700

    Updating calendarReducer to show added talk to calendar and remove from calendar

--- src/Redux/calendarReducer.js
+++ src/Redux/calendarReducer.js
@@ -3,9 +3,9 @@ export default (state = {}, { type, payload }) => {
     case 'LOAD_CALENDAR':
       return { ...state, calendar: payload };
     case 'ADD_EVENT':
-      return { ...state, calendar: payload };
-    case 'DELETE_ITEM':
-      return { ...state };
+      return { ...state,  calendar: [...state.calendar, payload]};
+    case 'DELETE_EVENT':
+      return { ...state, calendar: state.calendar.filter(event => event.id !== payload.id) };
     default:
       return state;
   }
